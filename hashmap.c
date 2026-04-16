6456110e93c718cd9fb20f6e9cd43a52f05192ef
Your Name <you@example.com>
2026-04-16 20:17:57 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -59,7 +59,7 @@ HashMap * createMap(long capacity) {
 // No inserte claves repetidas. Recuerde que el arreglo es circular. Recuerde actualizar la variable size.
 
 void insertMap(HashMap * map, char * key, void * value) {
-    if(map == NULL) return;
+    if(map -> capacity == NULL || map -> capacity == 0) return;
     int pos = hash(key, map -> capacity);
     while(map -> buckets[pos] != NULL || map -> buckets[pos] -> key != NULL){
         pos = (pos + 1) % map -> capacity;
